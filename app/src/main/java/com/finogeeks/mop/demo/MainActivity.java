86d3e6ab2e70a66361211539b99643c3f79f0c84
xiaoyu <yuriyshea@163.com>
2020-09-03 11:49:35 +0800

    集成最新稳定版sdk

--- app/src/main/java/com/finogeeks/mop/demo/MainActivity.java
+++ app/src/main/java/com/finogeeks/mop/demo/MainActivity.java
@@ -1,124 +1,36 @@
 package com.finogeeks.mop.demo;
 
 import android.os.Bundle;
-
-import com.finogeeks.lib.applet.client.FinAppClient;
-import com.finogeeks.lib.applet.client.FinAppConfig;
-import com.finogeeks.lib.applet.interfaces.FinCallback;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.Toast;
+import com.finogeeks.lib.applet.client.FinAppClient;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (FinAppClient.INSTANCE.isFinAppProcess(this)) {
-            return;
-        }
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FinAppConfig config = new FinAppConfig.Builder()
-                .setAppKey("22LyZEib0gLTQdU3MUauAVUHSFDdTToYDWCqr0AgPGwA")
-                .setAppSecret("8c5c3c26420b7e66")
-                .setApiUrl("https://mp.finogeeks.com")
-                .setApiPrefix("/api/v1/mop")
-                .setGlideWithJWT(false)
-                .build();
-// SDK初始化结果回调，用于接收SDK初始化状态
-        FinCallback<Object> callback = new FinCallback<Object>() {
-            @Override
-            public void onSuccess(Object result) {
-                // SDK初始化成功
-            }
 
-            @Override
-            public void onError(int code, String error) {
-                // SDK初始化失败
-                Toast.makeText(MainActivity.this, "SDK初始化失败", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onProgress(int status, String error) {
-
-            }
-        };
-        FinAppClient.INSTANCE.init(this.getApplication(), config, callback);
         Button canvasButton = findViewById(R.id.btnCanvas);
         Button demoButton = findViewById(R.id.btnDemo);
         Button profileButton = findViewById(R.id.btnProfile);
-        canvasButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                FinAppClient.INSTANCE.getAppletApiManager().startApplet(MainActivity.this, "5ea03fa563cb900001d73863",
-                        new HashMap(){
-                            {
-                                put("path","/pages/index/index");
-                            }
-                        });
-            }
+        canvasButton.setOnClickListener(v -> {
+            Map<String, String> startParams = new HashMap<>();
+            startParams.put("path", "/pages/index/index");
+            FinAppClient.INSTANCE.getAppletApiManager().startApplet(MainActivity.this, "5e3c147a188211000141e9b1", startParams);
         });
 
-        demoButton.setOnClickListener(new View.OnClickListener() {
+        demoButton.setOnClickListener(v -> FinAppClient.INSTANCE.getAppletApiManager().startApplet(MainActivity.this, "5e4d123647edd60001055df1"));
 
-            @Override
-            public void onClick(View v) {
-                FinAppClient.INSTANCE.getAppletApiManager().startApplet(MainActivity.this, "5ea0401463cb900001d73865");
-            }
-        });
-
-        profileButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                FinAppClient.INSTANCE.getAppletApiManager().startApplet(MainActivity.this, "5ea0412663cb900001d73867");
-            }
-        });
-
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FinAppClient.INSTANCE.getAppletApiManager().startApplet(MainActivity.this, "5e3c147a188211000141e9b1");
-//
-////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-////                        .setAction("Action", null).show();
-//            }
-//        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        profileButton.setOnClickListener(v -> FinAppClient.INSTANCE.getAppletApiManager().startApplet(MainActivity.this, "5f3e1c9d8d4295000144d400"));
     }
 }
---
+++ app/src/main/java/com/finogeeks/mop/demo/MopApplication.java
@@ -0,0 +1,55 @@
+package com.finogeeks.mop.demo;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.multidex.MultiDexApplication;
+
+import com.finogeeks.lib.applet.client.FinAppClient;
+import com.finogeeks.lib.applet.client.FinAppConfig;
+import com.finogeeks.lib.applet.interfaces.FinCallback;
+
+public class MopApplication extends MultiDexApplication {
+
+    private static final String TAG = "SampleApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (FinAppClient.INSTANCE.isFinAppProcess(this)) {
+            // 小程序进程不执行任何初始化操作
+            return;
+        }
+
+        FinAppConfig.UIConfig uiConfig = new FinAppConfig.UIConfig();
+        uiConfig.setHideNavigationBarCloseButton(true);
+        FinAppConfig config = new FinAppConfig.Builder()
+                .setAppKey(BuildConfig.APP_KEY)
+                .setAppSecret(BuildConfig.APP_SECRET)
+                .setApiUrl(BuildConfig.API_URL)
+                .setApiPrefix(BuildConfig.API_PREFIX)
+                .setDebugMode(BuildConfig.DEBUG)
+                .setGlideWithJWT(false)
+                .setUiConfig(uiConfig)
+                .setEncryptionType(FinAppConfig.ENCRYPTION_TYPE_SM)
+                .build();
+
+        FinAppClient.INSTANCE.init(this, config, new FinCallback<Object>() {
+            @Override
+            public void onSuccess(Object result) {
+                Log.d(TAG, "init result : " + result);
+            }
+
+            @Override
+            public void onError(int code, String error) {
+                Toast.makeText(MopApplication.this, "SDK初始化失败", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onProgress(int status, String error) {
+
+            }
+        });
+    }
+}
